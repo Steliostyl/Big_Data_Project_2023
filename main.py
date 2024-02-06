bff6951c2507eed452c7aad504439354660f2c6f
Steliostyl <52712681+Steliostyl@users.noreply.github.com>
2024-02-06 14:35:27 +0200

    Added create tables & updated diagrams

---
+++ main.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import db_connection
+
+def main():
+    #recipes_df = pd.read_csv(DATASET_PATH + "RAW_recipes.csv")
+    #print(recipes_df["ingredients"])
+    session = db_connection.connectToDB()
+    db_connection.createTables(session)
+    return
+
+if __name__ == "__main__":
+    main()
